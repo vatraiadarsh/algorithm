a2f6d102537286e93314f440045cfa34d0dabe1a
Adarsha <vatraiadarsh@gmail.com>
2022-06-30 12:43:36 +1000

    js algo

---
+++ js/mergeSort.js
@@ -0,0 +1,8 @@
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr
+    let mid = Math.floor(arr.length / 2)
+    // Recursive calls
+    let left = mergeSort(arr.slice(0, mid))
+    let right = mergeSort(arr.slice(mid))
+    return merge(left, right)
+  }
---
+++ manage.bat
@@ -0,0 +1,3 @@
+git add .
+git commit -m "js algo"
+git push -u origin main
